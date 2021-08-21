0f11f8073f1e74729c99af54a64cae00caba4bc2
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-08-21 18:17:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,17 +7,44 @@ function preload()
 
 function setup()
 {
-canvas=createCanvas(800,400);
+canvas=createCanvas(650,300);
 canvas.center();
-
-
-
-
+video=createCapture(VIDEO);
+video.hide();
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_Ah6R24TBX/model.json',model_loaded);
 }
+
+
 
 
 function draw()
 {
+    image(video,0,0,650,300);
+    classifier.classify(video,gotResult);
+    
+
+}
+
+function model_loaded()
+{
+console.log("Model is loaded ");
+
+}
+
+function gotResult(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+else
+{
+console.log(results);
+document.getElementById("result_family_member__name").innerHTML=results[0].label;
+document.getElementById("result_family_member_accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+}
 
 
 }
+
